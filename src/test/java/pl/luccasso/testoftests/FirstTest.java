12dbd9f88465dd0fb8b590b1d4eed97e2a473d15
pikolinian <pikolinian@gazeta.pl>
2020-03-03 11:41:52 +0100

    clean imports

--- src/test/java/pl/luccasso/testoftests/FirstTest.java
+++ src/test/java/pl/luccasso/testoftests/FirstTest.java
@@ -5,10 +5,9 @@
  */
 package pl.luccasso.testoftests;
 
-import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 /**
  *
